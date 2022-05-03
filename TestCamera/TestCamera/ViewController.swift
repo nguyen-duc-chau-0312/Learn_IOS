97f15616eb84d0cbf3326e3fbddfedc93d317c48
nguyen-duc-chau-0312 <nguyenducchau.1997@gmail.com>
2022-05-04 00:41:19 +0700

    Day Code

--- TestCamera/TestCamera/ViewController.swift
+++ TestCamera/TestCamera/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var pickedImage = false
+
+    override func viewDidAppear(_ animated: Bool) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) && !pickedImage {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+            pickedImage = true
+        }
     }
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imageView.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
 
